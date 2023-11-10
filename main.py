9ccadf817e7fab2a2c98cbca200fc33b0a51c367
Moad Hamousti <118165767+moadhamousti@users.noreply.github.com>
2023-11-10 20:56:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import warnings
 warnings.filterwarnings('ignore')
 import json
 import numpy as np
-
+ 
 st.set_page_config(page_title="Superstore!!!", page_icon=":bar_chart:",layout="wide")
 
 st.title(" :bar_chart: Sample SuperStore USA Departement")
